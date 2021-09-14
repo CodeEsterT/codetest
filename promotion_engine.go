e9eb0c8523361db55759641853e952fdc08e893a
Jan <jan@meznik.dk>
2021-09-14 19:03:24 +0200

    Rough draft for GetTotal()

--- promotion_engine.go
+++ promotion_engine.go
@@ -4,6 +4,30 @@ import (
 	"errors"
 )
 
-func GetTotal(items []string) (int, error) {
-	return 0, errors.New("Not implemented")
+type Promotion interface {
+	IsApplicable(items []string) bool
+	Apply(items []string) ([]string, int)
+}
+
+func GetTotal(price_table map[string]int, promotions []Promotion, items []string) (int, error) {
+	// Iterate over all promotions
+	for i := 0; i < len(promotions); i++ {
+		// Test if we can apply them
+		if promotions[i].IsApplicable(items) {
+			// Apply promotion
+			sub_items, promo_total := promotions[i].Apply((items))
+
+			// Recursively get the total for the rest of the order
+			sub_total, err := GetTotal(price_table, promotions, sub_items)
+
+			// Test for errors
+			if err != nil {
+				return 0, err
+			}
+
+			// Return
+			return sub_total + promo_total, nil
+		}
+	}
+	return 0, errors.New("not implemented")
 }
