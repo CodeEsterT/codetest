679489c2b68c4e9a689cf12684ac6d7c719464c2
Jan <jan@meznik.dk>
2021-09-14 18:37:50 +0200

    Added test for scenario C.

--- promotion_engine_test.go
+++ promotion_engine_test.go
@@ -28,3 +28,18 @@ func TestScenarioB(t *testing.T) {
 		t.Fatalf("Wrong total. Expected: %d, got: %d", expected_total, total)
 	}
 }
+
+func TestScenarioC(t *testing.T) {
+	order := []string{"A", "A", "A",
+		"B", "B", "B", "B", "B",
+		"C",
+		"D",
+	}
+	expected_total := 280
+
+	total, _ := GetTotal(order)
+
+	if total != expected_total {
+		t.Fatalf("Wrong total. Expected: %d, got: %d", expected_total, total)
+	}
+}
