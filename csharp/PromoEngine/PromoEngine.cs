898c992eb3e4b012ba7d7134d4470f96cc533fd3
Jan <jan@meznik.dk>
2021-09-29 19:39:47 +0200

    Added comments

--- csharp/PromoEngine/PromoEngine.cs
+++ csharp/PromoEngine/PromoEngine.cs
@@ -5,13 +5,29 @@ namespace PromoEngine
 {
     public interface IPromotion
     {
-        public bool IsApplicable(List<string> items);
+        /// <summary>
+        /// Returns true if the promotion applies to the cart. False otherwise.
+        /// </summary>
+        public bool IsApplicable(List<string> cart);
+
+        /// <summary>
+        /// Applies the promotion to the cart and returns a tuple of the updated cart 
+        /// and the price.
+        /// </summary>
         public Tuple<List<string>, double> Apply(List<string> cart);
     }
 
     public class BulkPromotion : IPromotion
     {
+        /// <summary>
+        /// Containes the list of items required to be present in the cart for the
+        /// promotion to apply.
+        /// </summary>
         List<string> requirements;
+
+        /// <summary>
+        /// The price of the promotion 
+        /// </summary>
         double price;
 
         public BulkPromotion(List<string> requirements, double price)
@@ -22,6 +38,7 @@ namespace PromoEngine
 
         public bool IsApplicable(List<string> items)
         {
+            // Iterates over the required items and check that they all are present
             List<string> item_check = new List<string>(items); 
 
             foreach(string item in requirements)
@@ -38,6 +55,8 @@ namespace PromoEngine
 
         public Tuple<List<string>, double> Apply(List<string> cart)
         {
+            // Remove the promotion items from the cart and return it in a new list
+            // together with the price
             List<string> new_cart = new List<string>(cart); 
  
             foreach(string item in requirements)
@@ -52,18 +71,14 @@ namespace PromoEngine
                 new_cart, price
             );
         }
-
-        public override string ToString()
-        {
-            return $"Promotion: {string.Join(',', requirements)} for {price}";
-        }
     }
 
     public class PromoEngine 
     {
-        List<IPromotion> promotions;
-        Dictionary<string, double> prices;
-
+        /// <summary>
+        /// Recursively iterate over the cart and apply promotions when possible.
+        /// If no promotions are applicable, sum up the rest of the items and return the price.
+        /// </summary>
         public double CalculatePrice(List<IPromotion> promotions, 
                                     Dictionary<string, double> prices,
                                     List<string> cart)
